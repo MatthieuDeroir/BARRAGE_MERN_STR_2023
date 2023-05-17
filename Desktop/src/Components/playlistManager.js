7df893cfd13ae42fc953a2dc750db96dcd278b38
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-05-17 16:04:46 +0200

    added image and data components + Playlistmanager

---
+++ Desktop/src/Components/playlistManager.js
@@ -0,0 +1,32 @@
+const ImageDisplay = require('./ImageDisplay');
+const DataDisplay = require('./DataDisplay');
+
+class PlaylistManager {
+	constructor(playlist) {
+		this.playlist = playlist;
+		this.currentIndex = 0;
+		this.imageDisplay = new ImageDisplay();
+		this.dataDisplay = new DataDisplay();
+	}
+
+	nextItem() {
+		let item = this.playlist[this.currentIndex];
+		try {
+			if (item.type === 'image') {
+				this.imageDisplay.show(item.content);
+				this.dataDisplay.hide();
+			} else if (item.type === 'data') {
+				this.dataDisplay.show(item.content);
+				this.imageDisplay.hide();
+			} else {
+				throw new Error('Invalid playlist item type');
+			}
+		} catch (error) {
+			console.error(error);
+		}
+
+		this.currentIndex = (this.currentIndex + 1) % this.playlist.length;
+	}
+}
+
+module.exports = PlaylistManager;
