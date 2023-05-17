d55a40ecf371f861bce5b377a72dd224400d533f
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-05-17 12:05:31 +0200

    first commit

---
+++ Frontend/src/Components/ConfigurationPanel/BroadcastButton 2.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function BroadcastButton() {
+    // TODO: Implement this
+    // the broadcast button should send a request to the backend to broadcast the current configuration
+    return (
+        <div>
+            <button>Broadcast</button>
+        </div>
+    );
+}
+
