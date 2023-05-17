9be821d051cdbeab147e57696362027a3efedca8
MatthieuDeroir <matthieu.deroir@epsi.fr>
2023-05-17 16:04:46 +0200

    added image and data components + Playlistmanager

--- Desktop/src/main.js
+++ Desktop/src/main.js
@@ -29,3 +29,22 @@ app.on('activate', () => {
         createWindow();
     }
 });
+
+const PlaylistManager = require('./Components/PlaylistManager');
+
+let playlist = [
+    { type: 'image', content: 'path/to/image1.jpg' },
+    { type: 'data', content: 'Hello, Electron!' },
+    // Add more items as needed
+];
+
+let playlistManager = new PlaylistManager(playlist);
+
+// Display the first item immediately
+playlistManager.nextItem();
+
+// Then cycle through the playlist every 5 seconds
+setInterval(() => {
+    playlistManager.nextItem();
+}, 5000);
+
