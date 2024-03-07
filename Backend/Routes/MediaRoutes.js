7994b5d617dc469df0e803f6e934fc16774424e4
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-07 15:01:48 +0100

    static

--- Backend/Routes/MediaRoutes.js
+++ Backend/Routes/MediaRoutes.js
@@ -8,7 +8,7 @@ const router = express.Router();
 
 //make a media static folder
 
-router.use('/media', express.static('media'));
+router.use('/', express.static('media'));
 router.post('/upload', authMiddleware.protect, upload.single('file'), uploadController.uploadFile);
 router.delete('/delete/:id', authMiddleware.protect,uploadController.deleteFile);
 router.put('/order', authMiddleware.protect,uploadController.updateOrder);
