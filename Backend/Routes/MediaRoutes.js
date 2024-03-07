e3917810a120d3c03bb852974e669cf916f0fec4
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-07 14:56:15 +0100

    added routes for media static folder

--- Backend/Routes/MediaRoutes.js
+++ Backend/Routes/MediaRoutes.js
@@ -6,6 +6,9 @@ const upload = require('../Middlewares/UploadMiddleware');
 const express = require('express');
 const router = express.Router();
 
+//make a media static folder
+
+router.use('/media', express.static('media'));
 router.post('/upload', authMiddleware.protect, upload.single('file'), uploadController.uploadFile);
 router.delete('/delete/:id', authMiddleware.protect,uploadController.deleteFile);
 router.put('/order', authMiddleware.protect,uploadController.updateOrder);
