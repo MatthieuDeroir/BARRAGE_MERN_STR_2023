d043b01aaabc0dfbe0e5353af880bdae8f5274db
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-08 13:12:59 +0100

    frontend data

--- Frontend/src/components/dashboard/preview/Preview.js
+++ Frontend/src/components/dashboard/preview/Preview.js
@@ -8,7 +8,57 @@ function Preview() {
   const [isOnline2, setIsOnline2] = useState(false);
   const [isOn2, setIsOn2] = useState(false);
 
-  // Supposons que vos hooks d'effet et la logique de fetch restent inchangés...
+ // Fetch les états de connexion au montage du composant
+ useEffect(() => {
+  const fetchSettingsAndSetStatus = async () => {
+    try {
+        // Récupérer les Datas using the dataService
+      const data = await DataService.getData();
+      console.log(data);
+      
+      setIsOnline1(data[0].client1Connected);
+      setIsOnline2(data[0].client2Connected);
+
+      console.log('Le client 1 est en ligne:', data[0].client1Connected);
+      console.log('Le client 2 est en ligne:', data[0].client2Connected);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des datas", error);
+    }
+    try {
+      
+
+      // Récupérer les paramètres using the settingsService
+      const settings = await settingsService.getSettings();
+      console.log(settings);
+      const now = new Date();
+      const startTime = new Date();
+      const endTime = new Date();
+
+      const [startHour, startMinute] = settings[0].start.split(':');
+      const [stopHour, stopMinute] = settings[0].stop.split(':');
+
+      startTime.setHours(parseInt(startHour, 10), parseInt(startMinute, 10));
+      endTime.setHours(parseInt(stopHour, 10), parseInt(stopMinute, 10));
+
+      let isActive = settings[0].enable && now <= startTime && now >= endTime;
+      console.log('Le paramètre de veille est activé:', isActive);
+
+      setIsOn1(!isActive);
+      setIsOn2(!isActive);
+    } catch (error) {
+      console.error("Erreur lors de la récupération des paramètres", error);
+    }
+  };
+
+  fetchSettingsAndSetStatus();
+
+  // Définir un intervalle pour rafraîchir les données toutes les minutes (60000ms)
+  const intervalId = setInterval(fetchSettingsAndSetStatus, 60000);
+
+  // Fonction de nettoyage pour arrêter l'intervalle lorsque le composant est démonté
+  return () => clearInterval(intervalId);
+}, []);
+
 
   return (
     <Grid item xs={12}>
