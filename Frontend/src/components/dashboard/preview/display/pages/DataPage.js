132185b9e8627f0ebd50692accec6076269ac9a7
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-08 14:07:09 +0100

    frontend data

--- Frontend/src/components/dashboard/preview/display/pages/DataPage.js
+++ Frontend/src/components/dashboard/preview/display/pages/DataPage.js
@@ -119,7 +119,7 @@ function DataPage({ waterData }) {
             wordWrap: "break-word",
           }}
         >
-          {waterData.cote_plan_eau || 0}
+          {waterData?.cote_plan_eau || 0}
         </div>
         <div
           style={{
@@ -150,7 +150,7 @@ function DataPage({ waterData }) {
             wordWrap: "break-word",
           }}
         >
-          {waterData.debit_entrant || 0}
+          {waterData?.debit_entrant || 0}
         </div>
         <div
           style={{
@@ -181,7 +181,7 @@ function DataPage({ waterData }) {
             wordWrap: "break-word",
           }}
         >
-          {waterData.debit_sortant || 0}
+          {waterData?.debit_sortant || 0}
         </div>
         <div
           style={{
