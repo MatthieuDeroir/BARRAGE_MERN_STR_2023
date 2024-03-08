541a561b934664f84ecf16a39c11def4694a2e49
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-08 12:38:09 +0100

    frontend data

--- Backend/Config/Websocket.js
+++ Backend/Config/Websocket.js
@@ -1,6 +1,6 @@
 const WebSocket = require("ws");
 const { updateClientStatus } = require("../Controllers/DataController");
-const { getActiveSlideshows, getSettings, getSlideshowStatus } = require("../Controllers/SlideshowController");
+const { getActiveSlideshows, getSettings, getSlideshowStatus } = require("./Parser");
 
 
 const wss = new WebSocket.Server({ host: '0.0.0.0', port: 8080 });
