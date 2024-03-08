66c10d03390611bd55b4537a79de4f8368d44087
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-08 13:58:20 +0100

    frontend data

--- Frontend/src/components/dashboard/preview/display/pages/DataPage.js
+++ Frontend/src/components/dashboard/preview/display/pages/DataPage.js
@@ -1,9 +1,7 @@
 import React, { useEffect } from "react";
 
 function DataPage({ waterData }) {
-  useEffect(() => {
-    console.log(waterData);
-  }, [waterData]);
+ 
 
   return (
     <div style={{ fontSize: "15px", display: "flex", flexDirection: "row" }}>
