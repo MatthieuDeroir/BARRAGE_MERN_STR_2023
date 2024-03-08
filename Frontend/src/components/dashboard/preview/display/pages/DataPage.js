226aef082394750cb490db7189c426fb902e6ef0
mderoir <mderoir@PC-MDR.stramatel.local>
2024-03-08 14:51:37 +0100

    frontend data

--- Frontend/src/components/dashboard/preview/display/pages/DataPage.js
+++ Frontend/src/components/dashboard/preview/display/pages/DataPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from "react";
+import "./DataPage.css";
 
 function DataPage({ waterData }) {
   useEffect(() => {
--- Frontend/src/components/dashboard/preview/display/pages/MediasPage.js
+++ Frontend/src/components/dashboard/preview/display/pages/MediasPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react'
 const API_URL = process.env.REACT_APP_API_URL;
+import './MediasPage.css';
 
 function MediasPage({ media }) {
 
